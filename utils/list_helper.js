9ad922b94b408b713f463dd9fb7cc1b550740bc3
Wafa Saefulhaq <saefulhaqwafa@gmail.com>
2023-12-11 15:50:56 +0700

    4.4 helper functions and unit tests step2

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -6,3 +6,14 @@
 export function dummy(blogs) {
 	return 1;
 }
+/**
+ *
+ * @param {{content: string, author: string, url: string, likes: number}[]} blogs
+ * @returns {number}
+ */
+export function totalLikes(blogs) {
+	return blogs.reduce((sum, blog) => {
+		// console.log('current blog likes >> ', blog.likes);
+		return sum + blog.likes;
+	}, 0);
+}
