0104281aabf47f3b03b660f2df87232c5b0ead8c
Wafa Saefulhaq <saefulhaqwafa@gmail.com>
2023-12-11 23:38:21 +0700

    4.6 helper functions and unit tests step4

--- tests/list_helper.test.js
+++ tests/list_helper.test.js
@@ -1,4 +1,10 @@
-import { dummy, favoriteBlog, totalLikes } from '../utils/list_helper';
+import {
+	dummy,
+	favoriteBlog,
+	mostBlogs,
+	totalLikes,
+	BLOGS,
+} from '../utils/list_helper';
 
 test('dummy return one', () => {
 	const blogs = [];
@@ -6,56 +12,6 @@ test('dummy return one', () => {
 	expect(result).toBe(1);
 });
 
-const blogs = [
-	{
-		_id: '5a422a851b54a676234d17f7',
-		title: 'React patterns',
-		author: 'Michael Chan',
-		url: 'https://reactpatterns.com/',
-		likes: 7,
-		__v: 0,
-	},
-	{
-		_id: '5a422aa71b54a676234d17f8',
-		title: 'Go To Statement Considered Harmful',
-		author: 'Edsger W. Dijkstra',
-		url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-		likes: 5,
-		__v: 0,
-	},
-	{
-		_id: '5a422b3a1b54a676234d17f9',
-		title: 'Canonical string reduction',
-		author: 'Edsger W. Dijkstra',
-		url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
-		likes: 12,
-		__v: 0,
-	},
-	{
-		_id: '5a422b891b54a676234d17fa',
-		title: 'First class tests',
-		author: 'Robert C. Martin',
-		url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
-		likes: 10,
-		__v: 0,
-	},
-	{
-		_id: '5a422ba71b54a676234d17fb',
-		title: 'TDD harms architecture',
-		author: 'Robert C. Martin',
-		url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
-		likes: 0,
-		__v: 0,
-	},
-	{
-		_id: '5a422bc61b54a676234d17fc',
-		title: 'Type wars',
-		author: 'Robert C. Martin',
-		url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
-		likes: 2,
-		__v: 0,
-	},
-];
 const listWithOneBlog = [
 	{
 		_id: '5a422aa71b54a676234d17f8',
@@ -79,7 +35,7 @@ describe('total likes', () => {
 	});
 
 	test('of a bigger list is calculated right', () => {
-		const result = totalLikes(blogs);
+		const result = totalLikes(BLOGS);
 		expect(result).toBe(36);
 	});
 });
@@ -91,8 +47,15 @@ describe('favorite blog', () => {
 			author: 'Edsger W. Dijkstra',
 			likes: 12,
 		};
-		const result = favoriteBlog(blogs);
+		const result = favoriteBlog(BLOGS);
 		console.log('result >> ', result);
 		expect(result).toEqual(blogWithMostLikes);
+	});
+});
+
+describe('most blogs', () => {
+	test('of author witsh most blogs', () => {
+		const result = mostBlogs(BLOGS);
+		expect(result).toEqual({ author: 'Robert C. Martin', blogs: 3 });
 	});
 });
