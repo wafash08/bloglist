e6ece35672d84992b5df480654c127405eb1c0c2
Wafa Saefulhaq <saefulhaqwafa@gmail.com>
2023-12-20 01:55:58 +0700

    5.23 bloglist e2e testing step7

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ import { MONGODB_URI } from './utils/config.js';
 import usersRouter from './controllers/users.js';
 import { errorHandler, tokenExtractor } from './utils/middleware.js';
 import loginRouter from './controllers/login.js';
+import testingRouter from './controllers/testing.js';
 
 mongoose.connect(MONGODB_URI);
 
@@ -18,15 +19,16 @@ morgan.token('blog', (req, res) => {
 	return JSON.stringify(req.body);
 });
 app.use(
-	morgan(
-		':method :url :status :res[content-length] - :response-time ms :blog'
-	)
+	morgan(':method :url :status :res[content-length] - :response-time ms :blog')
 );
 app.use(tokenExtractor);
 
 app.use('/api/blogs', blogRouter);
 app.use('/api/users', usersRouter);
 app.use('/api/login', loginRouter);
+if (process.env.NODE_ENV === 'test') {
+	app.use('/api/testing', testingRouter);
+}
 
 app.use(errorHandler);
 
